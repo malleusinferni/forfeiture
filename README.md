a0d55881deac1c59bea19f42cbf5abea76ccb72e
Mako <jlauve@rsmw.net>
2017-10-14 17:29:36 -0400

    Modified interface with testable example usage

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forfeiture
+
+This is a work-in-progress framework for hotloading game assets. It's mostly online as example code to be discussed on IRC. Don't expect it to do anything yet.
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,55 @@
-use std::sync::Arc;
+extern crate rayon_core;
 
-pub struct Keyed<K, V> where K: Key<Asset=V> {
-    key: K,
-    value: Option<Arc<V>>,
+use std::fmt::Debug;
+use std::hash::Hash;
+//use std::sync::Arc;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Error;
+
+pub type Result<T, E=Error> = std::result::Result<T, E>;
+
+pub trait Id: 'static + Clone + Debug + Eq + Hash {
+    type Target: Asset;
+
+    fn load(self) -> Result<Self::Target>;
 }
 
-pub trait Key: Sized {
-    type Asset;
+pub trait Asset: Default {
+    // Empty marker trait
+    // `Default` is required for the creation of placeholders
 }
 
-impl<K, V> Keyed<K, V> where K: Key<Asset=V> {
-    pub fn new(key: K) -> Self {
-        let value = None;
-        Keyed { key, value }
+#[cfg(test)]
+mod example {
+    use super::*;
+
+    use std::fs::File;
+    use std::io::{self, Read};
+    use std::path::PathBuf;
+
+    impl Id for PathBuf {
+        type Target = String;
+
+        fn load(self) -> Result<String> {
+            let mut buf = String::default();
+            File::open(&self)?.read_to_string(&mut buf)?;
+            Ok(buf)
+        }
     }
 
-    pub fn key(&self) -> &K {
-        &self.key
+    impl Asset for String {
     }
 
-    pub fn as_ref(&self) -> Option<&V> {
-        self.value.as_ref().map(|arc| arc.as_ref())
+    impl From<io::Error> for Error {
+        fn from(_err: io::Error) -> Self {
+            Error
+        }
+    }
+
+    #[test]
+    fn load_a_string() {
+        let path = PathBuf::from("README.md");
+        let _asset = Id::load(path).unwrap();
     }
 }
